7b11eb7f663584239328afc0413199a4ff3224f4
emiliarutkowska <erutkowska@student.agh.edu.pl>
2022-11-03 09:27:31 -0700

    Add test.

--- WebAppLabAzure/Program.cs
+++ WebAppLabAzure/Program.cs
@@ -3,6 +3,6 @@ var app = builder.Build();
 
 var configuration = (IConfiguration)app.Services.GetService(typeof(IConfiguration))!;
 
-app.MapGet("/", () => $"Hello World! Value: {configuration.GetSection("test").Value}");
+app.MapGet("/", () => $"FAILING TEST. Hello World! Value: {configuration.GetSection("test").Value}");
 
 app.Run();
